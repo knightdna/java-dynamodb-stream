b226a08b137c05d43a4d77625b4dc70540e68e6b
Diaz Novandi <apriandiaz_novandi@discovery.com>
2022-08-23 14:40:07 +0200

    Initialize project

---
+++ src/main/java/co/novandi/diaz/App.java
@@ -0,0 +1,13 @@
+package co.novandi.diaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
